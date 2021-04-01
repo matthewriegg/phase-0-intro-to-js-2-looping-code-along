8b1ef3f74298619a2c5c2baf8bf32194cdd9accc
Matthew Riegg <matthew.d.riegg@gmail.com>
2021-04-01 12:08:06 -0700

    Deliverables

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+
+const personName = [`Ada`, `Brendan`, `Ali`];
+const eventName = `birthday`;
+
+function writeCards(personName, eventName) {
+  let newArray = [];
+    for (let i = 0; i < personName.length; i++){
+        newArray.push(`Thank you, ${personName[i]}, for the wonderful ${eventName} gift!`)
+    }
+    debugger;
+  
+    return newArray;
+}
+
+    console.log(writeCards('personName', 'eventName'));
+
+function countDown(init = 10){
+    while (init >= 0){
+        console.log(init --);
+    }
+}
+
+
+
+
+
+
+
+
